15d2cfed6f5363ae6e7585618e8c8956da539690
Sabrine <bs.sabrina13@gmail.com>
2020-01-13 16:56:54 +0100

    sabrine-commit

---
+++ src/test-sabrine.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Testsabrine extends Component {
+    render() {
+        return (
+            <div>
+                Happy birthday to me :p
+            </div>
+        )
+    }
+}
